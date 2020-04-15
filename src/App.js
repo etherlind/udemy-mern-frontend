cc13be1d2fc6f36bf4b964abd62a4b5763d36ef1
camila <camila.yusupova@skitudeservices.com>
2020-04-15 10:33:11 +0200

    added routing configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/Place";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Users = () => {
+  return <h2 className="">Users works!</h2>;
+};
+export default Users;
