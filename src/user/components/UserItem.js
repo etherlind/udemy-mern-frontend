5dc5f58bc3cb7199353b384f7b26d1a676260237
camila <camila.yusupova@skitudeservices.com>
2020-04-16 10:37:49 +0200

    added the Google Map integration

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import Avatar from '../../shared/components/UIElements/Avatar';
 import Card from '../../shared/components/UIElements/Card';
-
 import './UserItem.css';
 
 const UserItem = props => {
