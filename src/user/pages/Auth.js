aedf10ad26516834cc6a7d88cf2d0706e4d076a1
camila <camila.yusupova@skitudeservices.com>
2020-04-22 11:31:15 +0200

    connected login to the backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -63,11 +63,34 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
+    setIsLoading(true);
+
     if (isLoginMode) {
-      //what
+      try {
+        // the fetch can return 4xx or 5xx without it being considered an error
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        // response is ok if the code is 2xx
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        auth.login();
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.message || "Something went wrong, please try again");
+      }
     } else {
       try {
-        setIsLoading(true);
         // the fetch can return 4xx or 5xx without it being considered an error
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
@@ -84,7 +107,6 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-        console.log(responseData);
 
         setIsLoading(false);
         auth.login();
